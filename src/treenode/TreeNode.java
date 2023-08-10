1ed801620a9e5f056218bb21ed8b1915f21a8880
NicoBar2 <nicoleyro13@gmail.com>
2023-08-10 18:55:14 -0500

    Gestor Tareas

---
+++ src/treenode/TreeNode.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package treenode;
+
+/**
+ *
+ * @author nicol
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+
+
